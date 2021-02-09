4222ba89b6f79e784a203ba3ade738f13d341a21
Duncan Palmer <duncan.pal@outlook.com>
2021-02-09 17:12:43 +1300

    Wrapped server list in row/col-md

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { wrapper } from "../redux";
 import { checkServerSideCookie } from "../redux/actions/auth";
-
 
 import Head from "next/head";
 
@@ -12,7 +11,6 @@ import Layout from "../hoc/Layout/Layout";
 import store from "../redux";
 
 export default function Home(props) {
-
 	const router = useRouter();
 
 	const serverClickedHandler = (id) => {
@@ -21,88 +19,110 @@ export default function Home(props) {
 
 	return (
 		<Layout authenticated={props.token ? true : false}>
-			<PagedTable
-				title="Sponsored Servers"
-				url="/servers/sponsored"
-				noDataMsg={(
-					<div className="col-md-12 mt-5">
-						<p className="mx-auto"><strong>There are currently no sponsored servers, <a href="/contact?subject=Server Sponsorship">click here</a> to enquire.</strong></p>
-					</div>
-				)}
-				page={1}
-				pageSize={5}
-				maxButtonsCount={6}
-				columnsRenderer={() => {
-					// ["Rank", "Name", "Server", "Players", "Status"]
-					return <React.Fragment>
-						<th>Rank</th>
-						<th className="d-sm-none">Name</th>
-						<th>Server</th>
-						<th>Players</th>
-						<th>Status</th>
-					</React.Fragment>
-				}}
-				itemRenderer={(dataItem) => {
-					return (
-						<Server
-							{...dataItem}
-							key={dataItem.id}
-							clicked={serverClickedHandler}
-						/>
-					);
-				}}
-			/>
+			<div className="row">
+				<div className="col-md-12">
+					<PagedTable
+						title="Sponsored Servers"
+						url="/servers/sponsored"
+						noDataMsg={
+							<div className="col-md-12 mt-5">
+								<p className="mx-auto">
+									<strong>
+										There are currently no sponsored
+										servers,{" "}
+										<a href="/contact?subject=Server Sponsorship">
+											click here
+										</a>{" "}
+										to enquire.
+									</strong>
+								</p>
+							</div>
+						}
+						page={1}
+						pageSize={5}
+						maxButtonsCount={6}
+						columnsRenderer={() => {
+							// ["Rank", "Name", "Server", "Players", "Status"]
+							return (
+								<React.Fragment>
+									<th>Rank</th>
+									<th className="d-sm-none">Name</th>
+									<th>Server</th>
+									<th>Players</th>
+									<th>Status</th>
+								</React.Fragment>
+							);
+						}}
+						itemRenderer={(dataItem) => {
+							return (
+								<Server
+									{...dataItem}
+									key={dataItem.id}
+									clicked={serverClickedHandler}
+								/>
+							);
+						}}
+					/>
+				</div>
+			</div>
 
-			<PagedTable
-				title="Server List"
-				url="/servers"
-				noDataMsg={(
-					<div className="col-md-12 mt-5">
-						<p className="mx-auto text-bold"><strong>There are currently no servers to list.</strong></p>
-					</div>
-				)}
-				page={1}
-				pageSize={10}
-				maxButtonsCount={6}
-				columnsRenderer={() => {
-					// ["Rank", "Name", "Server", "Players", "Status"]
-					return <React.Fragment>
-						<th>Rank</th>
-						<th className="d-md-table-cell d-sm-none">Name</th>
-						<th>Server</th>
-						<th>Players</th>
-						<th>Status</th>
-					</React.Fragment>
-				}}
-				itemRenderer={(dataItem) => {
-					return (
-						<Server
-							{...dataItem}
-							key={dataItem.id}
-							clicked={serverClickedHandler}
-						/>
-					);
-				}}
-			/>
+			<div className="row">
+				<div className="col-md-12">
+					<PagedTable
+						title="Server List"
+						url="/servers"
+						noDataMsg={
+							<div className="col-md-12 mt-5">
+								<p className="mx-auto text-bold">
+									<strong>
+										There are currently no servers to list.
+									</strong>
+								</p>
+							</div>
+						}
+						page={1}
+						pageSize={10}
+						maxButtonsCount={6}
+						columnsRenderer={() => {
+							// ["Rank", "Name", "Server", "Players", "Status"]
+							return (
+								<React.Fragment>
+									<th>Rank</th>
+									<th className="d-md-table-cell d-sm-none">
+										Name
+									</th>
+									<th>Server</th>
+									<th>Players</th>
+									<th>Status</th>
+								</React.Fragment>
+							);
+						}}
+						itemRenderer={(dataItem) => {
+							return (
+								<Server
+									{...dataItem}
+									key={dataItem.id}
+									clicked={serverClickedHandler}
+								/>
+							);
+						}}
+					/>
+				</div>
+			</div>
 		</Layout>
 	);
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
 	async (context) => {
-	  await checkServerSideCookie(context);
+		await checkServerSideCookie(context);
 
+		const token = context.store.getState().authReducer.token;
 
-	  const token = context.store.getState().authReducer.token;
-
-
-	  return {
-		props: {
-		  token,
-		},
-	  };
+		return {
+			props: {
+				token,
+			},
+		};
 	}
-  );
-  
-
-  
+);
