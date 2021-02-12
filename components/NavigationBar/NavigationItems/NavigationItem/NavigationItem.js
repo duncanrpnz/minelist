7b51a44f478c80f6074de0b432b53fb1e41cc9cd
Duncan Palmer <duncan.pal@outlook.com>
2021-02-12 18:28:42 +1300

    Links done

--- components/NavigationBar/NavigationItems/NavigationItem/NavigationItem.js
+++ components/NavigationBar/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import classes from "./NavigationItem.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const navigationItem = (props) => (
 	<li  className={classes.NavigationItem}>
-		<a href={props.href}>
+		<Link href={props.href} passHref><a>
 			<FontAwesomeIcon icon={props.icon} className="mr-2" />
 			{props.children}
-		</a>
+		</a></Link>
 	</li>
 );
 
